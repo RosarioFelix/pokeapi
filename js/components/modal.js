deaeeee81a1bf08bf1422bbeab63145a59605705
Rosario Felix <elijan_11@hotmail.com>
2017-07-20 08:34:09 -0500

    subiendo cambios modal

--- js/components/modal.js
+++ js/components/modal.js
@@ -1,13 +1,18 @@
 'use strict';
 
 const Modal = () => {
-    const modal  = $('<div id="modal1" class="modal">')
+    const modal  = $('<div id="modal1" class="modal">');
+    const containerModal  = $('<div class="container"></div>');
+    const rowModal  = $('<div class="row"></div>');
+    const colDoceModal = $('<div class="col s12"></div>');
     const contentDetail   = $('<div class="modal-content">');
     const modalFooter = $('<div class="modal-footer">');
     const modalClose = $('<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">X</a>');
 
     modal.append(contentDetail);
-    contentDetail
+    contentDetail.append(containerModal);
+    containerModal.append(rowModal);
+    rowModal.append(colDoceModal);
     modal.append(modalFooter);
     modalFooter.append(modalClose);
 
@@ -23,8 +28,8 @@ const Modal = () => {
   const colDoce = $('<div class="col m12"></div>');
   const titleModal  = $('<h2 class="center-align title">'+namePokemom+'</h2>');
   const rowDos    = $('<div class="row"></div>');
-  const imgColumn   = $('<div class="col s6 m4"></div>');
-  const detailsColumn = $('<div class="col s6 m8"></div>');
+  const imgColumn   = $('<div class="col s12 m4"></div>');
+  const detailsColumn = $('<div class="col s12 m8"></div>');
   const introPokemon  = $('<p class = "text">'+state.selectedStation.flavor_text_entries[1].flavor_text+'</p>');
   console.log(state.selectedStation.flavor_text_entries);
 
